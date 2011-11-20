4901fc0a1d8b7abeb7d46889c7f8987243560527
Daniel Vartanov <dan@vartanov.net>
2011-11-20 21:49:08 +0600

    Calculates route costs when user clicks two points on the map

--- application.rb
+++ application.rb
@@ -3,6 +3,17 @@ require "haml"
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
+def price(distance)
+  distance < 2 ?
+    80 :
+    80 + (distance - 2) * 10
+end
+
 get '/' do
   haml :index
+end
+
+get '/:distance' do
+  distance = params[:distance].to_f
+  price(distance).to_s
 end
